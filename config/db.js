7ff2de4bc1ae60865056663fbd3d913144ab15ed
matias <m.troncoso@saicoma.cl>
2020-05-08 13:37:04 -0400

    no testeado

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'variables.env' });
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect('mongodb+srv://Saitax:MPxWu31HpLCo9BKQ@cluster0-wrvwf.mongodb.net/merntask?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
