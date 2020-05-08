771633696617f41aa2d4faa36b7ba42d672aa4d1
matias <m.troncoso@saicoma.cl>
2020-05-08 14:32:44 -0400

    v1-production

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: 'variables.env'})
+require('dotenv').config({ path: 'variables.env' });
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect('mongodb+srv://Saitax:MPxWu31HpLCo9BKQ@cluster0-wrvwf.mongodb.net/merntask?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
+            useFindAndModify: false
         });
-        console.log('DB conectada');
+        console.log('DB Conectada');
     } catch (error) {
+        console.log('hubo un error')
         console.log(error);
         process.exit(1); // Detener la app
     }
--- routes/tareas.js
+++ routes/tareas.js
@@ -1,28 +1,36 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const tareaController = require("../controllers/tareaController");
-const auth = require("../middleware/auth");
-const { check } = require("express-validator");
+const tareaController = require('../controllers/tareaController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 // crear una tarea
 // api/tareas
-router.post(
-  "/",
-  auth,
-  [
-    check("nombre", "El nombre es obligatiorio").not().isEmpty(),
-    check("proyecto", "El proyecto es obligatorio").not().isEmpty(),
-  ],
-  tareaController.creaTarea
+router.post('/', 
+    auth,
+    [
+        check('nombre', 'El Nombre es obligatorio').not().isEmpty(),
+        check('proyecto', 'El Proyecto es obligatorio').not().isEmpty()
+    ],
+    tareaController.crearTarea
 );
 
-// obtener tareas por proyecto
-router.get("/", auth, tareaController.obtenerTarea);
+// Obtener las tareas por proyecto
+router.get('/',
+    auth,
+    tareaController.obtenerTareas
+);
 
-// actualizar tarea
-router.put("/:id", auth, tareaController.actualizarTarea);
+// Actualizar tarea
+router.put('/:id', 
+    auth,
+    tareaController.actualizarTarea
+);
 
-// eliminar tarea
-router.delete("/:id", auth, tareaController.eliminarTarea);
+// Eliminar tarea
+router.delete('/:id', 
+    auth,
+    tareaController.eliminarTarea
+);
 
 module.exports = router;
